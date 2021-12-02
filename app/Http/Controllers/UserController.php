977983dbac51f1cd5381af2f797b805b90a5aaa5
lucianovianna <luciano11v11@gmail.com>
2021-12-01 21:39:28 -0300

    Criando endpoint para editar usuario

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -2,17 +2,84 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;  
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Validator;
 
 class UserController extends Controller
 {
     private $loggedUser;
     
-    public function __contruct()
+    public function __construct()
     {
         $this->middleware("auth:api");
 
-        $this->loggedUser = auth()->user();
+        $this->loggedUser = Auth::user();
+    }
+
+    public function update(Request $request)
+    {
+        $data = $request->all();
+
+        $validator = Validator::make($data, [
+            'name'             => ['string', 'min:4', 'max:100'],
+            'email'            => ['email', 'unique:users,email', 'max:100'],
+            'password'         => ['string', 'min:4', 'max:100'],
+            'password_confirm' => ['string', 'min:4', 'max:100'],
+            'birthdate'        => ['date'],
+            'city'             => ['string', 'max:100'],
+            'work'             => ['string', 'max:100'],
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json([
+                "message" => $validator->errors(), 
+                "error" => true,
+            ], 400);
+        }
+        
+        $user = User::find($this->loggedUser["id"]);
+        $msg = [];
+
+        if (isset($data["name"])) {
+            $user->name = $data["name"];
+            $msg[] = "Nome atualizado.";
+        }
+        if (isset($data["email"]) && $data["email"] != $user->email) {
+            $user->email = $data["email"];
+            $msg[] = "Email atualizado.";
+        }
+        if (isset($data["birthdate"])) {
+            $user->birthdate = $data["birthdate"];
+            $msg[] = "Data de nascimento atualizado.";
+        }
+        if (isset($data["city"])) {
+            $user->city = $data["city"];
+            $msg[] = "Cidade atualizado.";
+        }
+        if (isset($data["work"])) {
+            $user->work = $data["work"];
+            $msg[] = "Trabalho atualizado.";
+        }
+        if (isset($data["password"])) {
+            if (isset($data["password_confirm"]) && $data["password"] === $data["password_confirm"]) {
+                $user->password = password_hash($data["password"], PASSWORD_DEFAULT);
+                $msg[] = "Senha atualizada.";
+            } else {
+                return response()->json([
+                    "message" => "As senhas não batem.", 
+                    "error" => true
+                ], 400);
+            }
+        }
+
+        $user->save();
+        $msg[] = "Usuario salvo com sucesso.";
+
+        return response()->json([
+            "error" => false,
+            "messages" => $msg,
+        ]);
     }
 }
