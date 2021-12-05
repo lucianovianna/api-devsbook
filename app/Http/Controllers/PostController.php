b9bd4ef48f317803de21184a567c3b2d7dce2d57
lucianovianna <luciano11v11@gmail.com>
2021-12-05 12:06:09 -0300

    Criando endpoint para dar like em uma postagem

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -2,8 +2,10 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Http\Request;
+use App\Models\PostLike;
+use App\Models\Post;
 
 class PostController extends Controller
 {
@@ -14,5 +16,40 @@ class PostController extends Controller
         $this->middleware("auth:api");
 
         $this->loggedUser = auth()->user();
+    }
+
+    public function like($id)
+    {
+        $messages = [];
+
+        if (!Post::find($id)) {
+            return response()->json(["error" => "Post inexistente"], 400);
+        }
+
+        $isLiked = PostLike::where("id_post", $id)
+            ->where("id_user", $this->loggedUser["id"])
+            ->count();
+        
+        if ($isLiked > 0) {
+            $postLike = PostLike::where("id_post", $id)
+                ->where("id_user", $this->loggedUser["id"])
+                ->first();
+            
+            $postLike->delete();
+
+            $messages["isLiked"] = false;
+        } else {
+            $newPostLike = new PostLike();
+            $newPostLike->id_post = $id;
+            $newPostLike->id_user = $this->loggedUser["id"];
+            $newPostLike->created_at = date("Y-m-d H:i:s");
+            $newPostLike->save();
+            
+            $messages["isLiked"] = true;
+        }
+
+        $messages["likeCout"] = PostLike::where("id_post", $id)->count();
+
+        return response()->json([$messages]);
     }
 }
