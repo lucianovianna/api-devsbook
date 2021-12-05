e3cb20509ffe40864b1965bde9a9179b1d53398c
lucianovianna <luciano11v11@gmail.com>
2021-12-05 12:28:44 -0300

    Criando endpoint para buscar usuarios

--- app/Http/Controllers/SearchController.php
+++ app/Http/Controllers/SearchController.php
@@ -2,8 +2,10 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Http\Request;
+use App\Models\User;
 
 class SearchController extends Controller
 {
@@ -14,5 +16,32 @@ class SearchController extends Controller
         $this->middleware("auth:api");
 
         $this->loggedUser = auth()->user();
+    }
+
+    public function search(Request $request)
+    {
+        $messages = ["users" => []];
+
+        $data = $request->all();
+        $validator = Validator::make($data, [
+            "txt" => ["required", "string"]
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json(["messages" => $validator->errors()], 400);
+        }
+
+        // Busca de usuarios
+        $userList = User::where("name", "like", "%".$data["txt"]."%")->get();
+
+        foreach($userList as $userItem) {
+            $messages["users"][] = [
+                "id" => $userItem["id"],
+                "name" => $userItem["name"],
+                "avatar" => url("media/avatars/" . $userItem["avatar"])
+            ];
+        }
+
+        return response()->json([$messages]);
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -46,4 +46,4 @@ Route::post("/feed", [FeedController::class, "create"]);
 Route::post("/post/{id}/like", [PostController::class, "like"]);
 Route::post("/post/{id}/comment", [PostController::class, "comment"]);
 
-// Route::get("/search", [SearchController::class, "search"]);
+Route::get("/search", [SearchController::class, "search"]);
